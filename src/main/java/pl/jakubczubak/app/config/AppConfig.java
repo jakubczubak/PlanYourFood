641af1cc5b9a0781afe619f9c7627c05a2d2ad6e
Jakub Czubak <czubakjakub94@gmail.com>
2020-01-01 15:45:20 +0100

    add Hibernate and Spring Data

--- src/main/java/pl/jakubczubak/app/config/AppConfig.java
+++ src/main/java/pl/jakubczubak/app/config/AppConfig.java
@@ -3,13 +3,21 @@ package pl.jakubczubak.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories( basePackageClasses = BookRepository.class)
 @ComponentScan(basePackages = "pl.jakubczubak.app")
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
@@ -18,4 +26,14 @@ public class AppConfig extends WebMvcConfigurerAdapter {
                 new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
-        return viewResolver; } }
+        return viewResolver; }
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("foodappPersistenceUnit");
+        return emfb; }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm; }
+}
--- src/main/java/pl/jakubczubak/app/model/Book.java
+++ src/main/java/pl/jakubczubak/app/model/Book.java
@@ -1,23 +1,24 @@
 package pl.jakubczubak.app.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String author;
     private String isbn;
 
-    public Book(int id, String title, String author, String isbn) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
-
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -44,4 +45,5 @@ public class Book {
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
 }
